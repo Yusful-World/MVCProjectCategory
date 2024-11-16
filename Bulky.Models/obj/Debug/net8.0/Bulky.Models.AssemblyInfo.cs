c394ae6b46dac04ffdff05752090418f065ffd62
Abdullahi Yusuf <abdullahiyusuf182@gmail.com>
2024-11-16 18:33:13 +0100

    ..

--- Bulky.Models/obj/Debug/net8.0/Bulky.Models.AssemblyInfo.cs
+++ Bulky.Models/obj/Debug/net8.0/Bulky.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f97cde49f3f1194ab5187fbc2ca75c88304bf764")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c06858bf5aad981fab66d1b3eee054ce2af3fa69")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
