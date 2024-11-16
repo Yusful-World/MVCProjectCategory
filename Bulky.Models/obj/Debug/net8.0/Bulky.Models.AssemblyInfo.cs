873ac60456950c97f1deef084e40a765040238d3
Abdullahi Yusuf <abdullahiyusuf182@gmail.com>
2024-11-16 20:05:45 +0100

    resolved conflicts

--- Bulky.Models/obj/Debug/net8.0/Bulky.Models.AssemblyInfo.cs
+++ Bulky.Models/obj/Debug/net8.0/Bulky.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c06858bf5aad981fab66d1b3eee054ce2af3fa69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35dfd5a3d313a37e333888845fe0d01f94aed6ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
