cbb3b8a5b0b5e832526b7d21426089df27810e74
Abdullahi Yusuf <abdullahiyusuf182@gmail.com>
2024-11-19 20:03:45 +0100

    Created ViewModel

--- Bulky.DataAccess/obj/Debug/net8.0/Bulky.DataAccess.AssemblyInfo.cs
+++ Bulky.DataAccess/obj/Debug/net8.0/Bulky.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98d1c7bb6c32e3bb0b6fe5a0c19185df325aad45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4e664c44982fbc4ede1a9426b82e5d01602bd08")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
