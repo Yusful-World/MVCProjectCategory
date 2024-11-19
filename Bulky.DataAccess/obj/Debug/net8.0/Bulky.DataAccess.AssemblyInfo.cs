e984a187558fb42770995a873341508939b74969
Abdullahi Yusuf <abdullahiyusuf182@gmail.com>
2024-11-20 00:16:54 +0100

    Did minor modifications

--- Bulky.DataAccess/obj/Debug/net8.0/Bulky.DataAccess.AssemblyInfo.cs
+++ Bulky.DataAccess/obj/Debug/net8.0/Bulky.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03128499ad91c3187bbb95410d9f40015c793e5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98d1c7bb6c32e3bb0b6fe5a0c19185df325aad45")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Bulky.Models/obj/Debug/net8.0/Bulky.Models.AssemblyInfo.cs
+++ Bulky.Models/obj/Debug/net8.0/Bulky.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e15876886d8690665e65a49f5a60e41a9906a578")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98d1c7bb6c32e3bb0b6fe5a0c19185df325aad45")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
