f42795e13199b873112168b72575a2eff885fa24
Abdullahi Yusuf <abdullahiyusuf182@gmail.com>
2024-11-27 05:30:21 +0100

    Modified register and log in UI

--- BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de8660aa0f72a0516a6dbcf2e4e3dc725abcfbb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca22430b735e24e655637aad2e471779de1623c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
